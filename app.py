3d463403971efed3b7fd8be5574512236da0b19e
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-07-17 13:51:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     youtube_videoid = request.args.get('youtube_videoid')
-    if not youtube_videoid:
-        return jsonify({"error": "No youtube_videoid provided"}), 400
+    videoid_audioonly = request.args.get('videoid_audioonly')
+    
+    if youtube_videoid:
+        return get_video_url(youtube_videoid)
+    elif videoid_audioonly:
+        return get_audio_url(videoid_audioonly)
+    else:
+        return jsonify({"error": "No videoid provided"}), 400
 
-    youtube_url = f'https://www.youtube.com/watch?v={youtube_videoid}'
+def get_video_url(videoid):
+    youtube_url = f'https://www.youtube.com/watch?v={videoid}'
     try:
         ydl_opts = {
             'format': 'best',
@@ -24,6 +31,31 @@ def home():
                 return jsonify({"error": "Could not retrieve video URL"}), 500
 
         return jsonify({"video_url": video_url})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+def get_audio_url(videoid):
+    youtube_url = f'https://www.youtube.com/watch?v={videoid}'
+    try:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'noplaylist': True,
+            'quiet': True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(youtube_url, download=False)
+            audio_url = info_dict.get('url', None)
+            if not audio_url:
+                return jsonify({"error": "Could not retrieve audio URL"}), 500
+
+        return jsonify({"audio_url": audio_url})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
